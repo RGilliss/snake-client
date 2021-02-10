dde5d6a59fc86dfb851d73ee8f1b2efd254ee3ab
Rye Gilliss <gilliss.rg@gmail.com>
2021-02-10 21:21:48 +0000

    input added

--- play.js
+++ play.js
@@ -1,16 +1,21 @@
 const { connect } = require('./client');
-console.log('Connecting ...');
-connect();
 
-const setupInput = () => {
-  const handleUserInput = (data) => {
-    if (data === '\u0003') {
-      process.exit();
-    }
-  };
+
+console.log('Connecting ...');
+
+const handleUserInput = (input) => {
+  if (input === '\u0003') {
+    process.exit();
+  }
+};
+const setupInput = function() {
+  
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
+  stdin.on('data', handleUserInput);
   stdin.resume();
   return stdin;
 }
+
+setupInput(connect());
