918e772a3af5246d57d520246fbe1f8b7ede7689
Rye Gilliss <gilliss.rg@gmail.com>
2021-02-10 20:27:13 +0000

    set up UI

--- play.js
+++ play.js
@@ -1,3 +1,16 @@
 const { connect } = require('./client');
 console.log('Connecting ...');
 connect();
+
+const setupInput = () => {
+  const handleUserInput = (data) => {
+    if (data === '\u0003') {
+      process.exit();
+    }
+  };
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  return stdin;
+}
