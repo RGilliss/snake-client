13c9593c338d11ffae062cd6308cd594373b1e75
Rye Gilliss <gilliss.rg@gmail.com>
2021-02-10 22:37:36 +0000

    Movement added

--- input.js
+++ input.js
@@ -1,15 +1,31 @@
+let connection;
+
 const handleUserInput = (input) => {
-  if (input === '\u0003') {
+  if (input === '\u0003') { // ctrl + c
     process.exit();
   }
+  if (input === "w") {
+    connection.write('Move: up');
+  } 
+  if (input === 'a') {
+    connection.write('Move: left');
+  }
+  if (input === 'd') {
+    connection.write('Move: right');
+  }
+  if (input === 's') {
+    connection.write('Move: down');
+  }
 };
-const setupInput = function() {
-  
+const setupInput = function(conn) {
   const stdin = process.stdin;
+  connection = conn;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
-  stdin.on('data', handleUserInput);
   stdin.resume();
+  stdin.on('data', (data) => {
+    handleUserInput(data);
+  });
   return stdin;
 }
 
